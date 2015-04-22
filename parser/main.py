92469456222a8d5c00595ca34ea21c66042a5040
Piotr Bujakowski <piotr145@v-lo.krakow.pl>
2015-04-22 08:32:55 +0200

    modify main loop

--- parser/main.py
+++ parser/main.py
@@ -7,7 +7,12 @@ while True:
     input_record = pipeout.readline()
     if input_record.split(',')[0] == '$GYRO':
         gyro = input_record
+	if input_record.split(',')[0] == '$ACCEL':
         accel = pipeout.readline()
+	if input_record.split(',')[0] == '$MAGNET':
         magnet = pipeout.readline()
+	if input_record.split(',')[0] == '$MBAR':
         pressure = pipeout.readline()
-        parse.parse_IMU(gyro, accel, magnet, pressure)
+	if all([gyro, accel, magnet, pressure]):
+		parse.parse_IMU(gyro, accel, magnet, pressure)
+		gyro = accel = magnet = pressure = None
