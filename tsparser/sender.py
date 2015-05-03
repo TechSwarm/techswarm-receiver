e1df16a2b6adddcb93d8fd41a19b99a24209566f
Piotr Bujakowski <piotrb2727@gmail.com>
2015-05-03 22:11:31 +0200

    move from urllib to requests library

--- tsparser/sender.py
+++ tsparser/sender.py
@@ -1,6 +1,4 @@
-import urllib.request
-import urllib.parse
-from tsparser.auth import generate_auth
+import requests
 from tsparser import config
 
 
@@ -15,14 +13,9 @@ def send_data(data, url):
     :return True if data have been sent successfully; False otherwise
     :rtype bool
     """
-    header = {
-        'Authorization': generate_auth(config.USERNAME, config.PASSWORD)
-        }
-
-    payload = urllib.parse.urlencode(data).encode('utf-8')
-    request = urllib.request.Request(url, payload, header)
     try:
-        response = urllib.request.urlopen(request)
-    except urllib.request.HTTPError:
+        response = requests.post(url, data=data,
+                                 auth=(config.USERNAME, config.PASSWORD))
+    except requests.HTTPError:
         return False
-    return response == 201
+    return response.status_code == 201
