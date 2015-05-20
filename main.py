991ffc60509616c8d33be6b4a08ea5747f2d5752
Artur Jamro <artur.jamro@gmail.com>
2015-05-20 14:37:56 +0200

    Added catching system (parsers) errors and displaying them

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from time import sleep
+
 from tsparser import main
 from tsparser.ui import UserInterface
+from tsparser.utils.statistic_data_collector import StatisticDataCollector
 
 UserInterface().run()
-main.parse()
+try:
+    main.parse()
+except Exception as err:
+    logger = StatisticDataCollector().get_logger()
+    logger.log('system', '{}: {}'.format(err.__class__.__name__, err))
+    logger.log('system', 'System has crashed. Please exit manually.')
+    while True:
+        sleep(1)
