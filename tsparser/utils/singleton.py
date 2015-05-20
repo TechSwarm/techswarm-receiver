7e8a3ecf5721df601d1d3a4601cca8ad585b67cb
Artur Jamro <artur.jamro@gmail.com>
2015-05-20 18:57:00 +0200

    Singleton is now thread-safe (fixed bug with logger)

--- tsparser/utils/singleton.py
+++ tsparser/utils/singleton.py
@@ -1,7 +1,18 @@
+from threading import Lock
+
+
 class Singleton(type):
     _instances = {}
+    _lock = Lock()
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+        try:
+            cls._lock.acquire()
+            if cls not in cls._instances:
+                cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._lock.release()
+            return cls._instances[cls]
+        except:
+            if cls._lock.locked():
+                cls._lock.release()
+            raise
